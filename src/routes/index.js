d805d1d6057f270f054cf0046e9ee90c80d2268d
gabriel justo <gjusto@unsa.edu.pe>
2020-05-05 09:27:27 -0500

    creando vistas, rutas de productos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
-const express=require('express'); 
+const express = require('express');
 const router = express.Router();
-router.get('/',(req,res)=>{
+const { isLoggedIn } = require('../lib/auth');
+
+router.get('/', isLoggedIn, (req, res) => {
     res.send("hello world");
 });
-module.exports =router; 
+module.exports = router;
--- src/routes/roles.js
+++ src/routes/roles.js
@@ -1,72 +1,71 @@
 const express = require('express');
-const router=express.Router();
+const router = express.Router();
 
 const pool = require('../database');
- router.get('/add',(req,res)=>{
-     res.render('roles/add');
- });
- router.post('/add',async(req,res)=>{
-     const {name,estado}= req.body;
-     const newRol={
+const { isLoggedIn } = require('../lib/auth');
+router.get('/add', isLoggedIn, (req, res) => {
+    res.render('roles/add');
+});
+router.post('/add', isLoggedIn, async(req, res) => {
+    const { name, estado } = req.body;
+    const newRol = {
         name,
         estado
-     }; 
-    const roles=await pool.query("select * from rol where name=?",[name]);
-    let rolesTotal=roles.length;
- 
-     if(rolesTotal>0){
-        
+    };
+    const roles = await pool.query("select * from rol where name=?", [name]);
+    let rolesTotal = roles.length;
+
+    if (rolesTotal > 0) {
+
         res.render('roles/add');
-    }
-    else{
-        await pool.query('INSERT INTO rol set ?',[newRol]);
-        req.flash('guardado','Guardado correctamente');
+    } else {
+        await pool.query('INSERT INTO rol set ?', [newRol]);
+        req.flash('guardado', 'Guardado correctamente');
         res.redirect('/roles');
-     }
-   
-    
- })
-router.get('/',async(req,res)=>{
-    const roles=await pool.query('select * from rol');
-    res.render('roles/list',{roles});
+    }
+
+
+})
+router.get('/', isLoggedIn, async(req, res) => {
+    const roles = await pool.query('select * from rol');
+    res.render('roles/list', { roles });
 });
-router.get('/delete/:id',async(req,res)=>{
-    const {id} = req.params;
+router.get('/delete/:id', isLoggedIn, async(req, res) => {
+    const { id } = req.params;
     // await pool.query("update rol set estado='*' where id=?",[id]);
-    await pool.query("delete from rol where id=?",[id]);
-    req.flash('guardado','rol eliminado satisfactoriamente')
+    await pool.query("delete from rol where id=?", [id]);
+    req.flash('guardado', 'rol eliminado satisfactoriamente')
     res.redirect('/roles');
 })
-router.get('/edit/:id',async(req,res)=>{
-    const {id} = req.params;
-    const rol=await pool.query("select * from rol where id=?",[id]);
-    res.render('roles/edit',{rol: rol[0]});
+router.get('/edit/:id', isLoggedIn, async(req, res) => {
+    const { id } = req.params;
+    const rol = await pool.query("select * from rol where id=?", [id]);
+    res.render('roles/edit', { rol: rol[0] });
 })
-router.post('/edit/:id',async(req,res)=>{
-    const {id} = req.params;
-    const{name,estado} = req.body;
-    const rol={
-        name,estado
+router.post('/edit/:id', isLoggedIn, async(req, res) => {
+    const { id } = req.params;
+    const { name, estado } = req.body;
+    const rol = {
+        name,
+        estado
     };
-    
-    const roles=await pool.query("select * from rol where name=?",[name]);
-    const nameActOld=await pool.query("select * from rol where id=?",[id]);
-    let rolesTotal=roles.length;
-    if(nameActOld[0].name!=name){
-        if(rolesTotal>0){
+
+    const roles = await pool.query("select * from rol where name=?", [name]);
+    const nameActOld = await pool.query("select * from rol where id=?", [id]);
+    let rolesTotal = roles.length;
+    if (nameActOld[0].name != name) {
+        if (rolesTotal > 0) {
             res.send("rol ya existe");
             // ('rol ya existe')
-        }
-        else{
-            await pool.query("update rol set ? where id=?",[rol,id]);
-            req.flash('guardado','rol editado satisfactoriamente')
+        } else {
+            await pool.query("update rol set ? where id=?", [rol, id]);
+            req.flash('guardado', 'rol editado satisfactoriamente')
             res.redirect('/roles');
         }
-    }
-    else{
-        await pool.query("update rol set ? where id=?",[rol,id]);
-        req.flash('guardado','rol editado satisfactoriamente')
+    } else {
+        await pool.query("update rol set ? where id=?", [rol, id]);
+        req.flash('guardado', 'rol editado satisfactoriamente')
         res.redirect('/roles');
     }
 })
-module.exports = router; 
+module.exports = router;
